78a6b388dfbe7d04fb1269413ff76a60c7ca33b0
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-05-22 17:07:31 +0330

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub langgrammer); // synthesized by LALRPOP
+
 fn main() {
-    println!("Hello, world!");
+    match langgrammer::TermParser::new().parse("22") {
+        Ok(v) => {
+            assert_eq!(v, 22);
+            println!("Success");
+        }
+        Err(err) => {
+            println!("Err: {}", err);
+        }
+    }
+    
+    assert!(langgrammer::TermParser::new().parse("22").is_ok());
+    assert!(langgrammer::TermParser::new().parse("(22)").is_ok());
+    assert!(langgrammer::TermParser::new().parse("((((22))))").is_ok());
+    assert!(langgrammer::TermParser::new().parse("((22)").is_err());
 }
